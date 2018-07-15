56ffcdda7d5436794421ec960e0e089191d4bbcb
Leonardo Vianna <lvfeiteira@inf.ufrgs.br>
2018-07-14 23:46:03 -0300

    Now the app.js works and its showing the pages

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+// let cookieParser = require('cookie-parser');
 
 let app = express();
 
 let port = process.env.PORT || 3000;
 
+let nav = [
+  {
+    link: '/',
+    text: 'Home'
+  },
+  {
+    link: '/signup',
+    text: 'Sign Up'
+  },
+  {
+    link: '/account',
+    text: 'My Account'
+  },
+  {
+    link: '/contact',
+    text: 'Contact Us'
+  }
+];
+
+let accountRouter = require('./src/routes/accountRoutes.js') (nav);
+
+/* Use everything from /public/ as static */
 app.use(express.static('public'));
+/* Parser any request as a json*/
+app.use(bodyParser.json());
+/* Extended:true means a more complex parser algorithm*/
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.use('/account', accountRouter);
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home',
+    nav: nav
+  });
+});
 
 app.listen(port, (err) => {
   if (err) {
